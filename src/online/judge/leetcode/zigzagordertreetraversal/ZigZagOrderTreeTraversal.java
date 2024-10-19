3b5461a719a6d3e013519c0f13eee3292bb5c897
adnanrahin <adnan.rahin13@gmail.com>
2024-10-19 18:57:40 -0400

    Binary Tree Level Ordre Traversal

---
+++ src/online/judge/leetcode/zigzagordertreetraversal/ZigZagOrderTreeTraversal.java
@@ -0,0 +1,66 @@
+package online.judge.leetcode.zigzagordertreetraversal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ZigZagOrderTreeTraversal {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+            if (root == null) return new ArrayList<>();
+
+            List<List<Integer>> result = new ArrayList<>();
+
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            int level = 0;
+
+            while (!queue.isEmpty()) {
+
+                int size = queue.size();
+                List<Integer> local = new ArrayList<>();
+                for (int i = 0; i < size; i++) {
+                    TreeNode u = queue.poll();
+                    if (level % 2 == 0) {
+                        local.add(u.val);
+                    } else {
+                        local.add(0, u.val);
+                    }
+                    if (u.left != null) queue.add(u.left);
+                    if (u.right != null) queue.add(u.right);
+                }
+                result.add(local);
+                level++;
+
+            }
+            return result;
+        }
+    }
+
+}
