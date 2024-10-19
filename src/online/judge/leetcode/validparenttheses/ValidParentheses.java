edbf420f065290c11e044b8ebdd5ab3b58f05eec
adnanrahin <adnan.rahin13@gmail.com>
2024-10-19 03:09:12 -0400

    update

---
+++ src/online/judge/leetcode/validparenttheses/ValidParentheses.java
@@ -0,0 +1,45 @@
+package online.judge.leetcode.validparenttheses;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+            } else {
+                char top = s.charAt(i);
+                if (!stack.isEmpty()) {
+                    if (top == ']') {
+                        if (stack.pop() != '[') {
+                            return false;
+                        }
+                    } else if (top == '}') {
+                        if (stack.pop() != '{') {
+                            return false;
+                        }
+                    } else if (top == ')') {
+                        if (stack.pop() != '(') {
+                            return false;
+                        }
+                    }
+                } else return false;
+            }
+
+        }
+
+        return stack.isEmpty();
+    }
+}
