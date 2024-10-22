4bf5e5e1f94f2ac8d54876e0177507aa71989fba
adnanrahin <adnan.rahin13@gmail.com>
2024-10-22 02:46:41 -0400

    Combination Sum

---
+++ src/online/judge/leetcode/backtrack/combinationsum/CombinationSum.java
@@ -0,0 +1,43 @@
+package online.judge.leetcode.backtrack.combinationsum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+
+    public static void main(String[] args) {
+
+        System.out.println(new Solution().combinationSum(new int[]{2, 3, 6, 7}, 7));
+
+    }
+
+}
+
+class Solution {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        backTrack(candidates, target, 0, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    public void backTrack(int[] candidates, int target, int index, List<Integer> combination, List<List<Integer>> res) {
+
+        if (target < 0) return;
+
+        if (target == 0) {
+            res.add(new ArrayList<>(combination));
+            return;
+        }
+
+        for (int i = index; i < candidates.length; i++) {
+            combination.add(candidates[i]);
+            backTrack(candidates, target - candidates[i], i, combination, res);
+            combination.remove(combination.size() - 1);
+        }
+    }
+
+}
