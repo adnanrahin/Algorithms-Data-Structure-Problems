bec831685855553ca57d3b089b654f60fffc0a47
adnanrahin <adnan.rahin13@gmail.com>
2024-10-22 01:40:15 -0400

    update

---
+++ src/online/judge/leetcode/linkedlists/mergetwosortedlists/MergeTwoSortedLists.java
@@ -0,0 +1,62 @@
+package online.judge.leetcode.linkedlists.mergetwosortedlists;
+
+public class MergeTwoSortedLists {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+            ListNode result = new ListNode(0);
+            ListNode head = result;
+            while (list1 != null && list2 != null) {
+                if (list1.val > list2.val) {
+                    result.next = new ListNode(list2.val);
+                    result = result.next;
+                    list2 = list2.next;
+                } else if (list1.val < list2.val) {
+                    result.next = new ListNode(list1.val);
+                    result = result.next;
+                    list1 = list1.next;
+                } else {
+                    result.next = new ListNode(list1.val);
+                    result = result.next;
+                    result.next = new ListNode(list2.val);
+                    result = result.next;
+                    list1 = list1.next;
+                    list2 = list2.next;
+                }
+            }
+
+            ListNode reminder = (list1 == null) ? list2 : list1;
+
+            while (reminder != null) {
+                result.next = new ListNode(reminder.val);
+                result = result.next;
+                reminder = reminder.next;
+            }
+
+            return head.next;
+        }
+    }
+
+}
+
+
+
+
