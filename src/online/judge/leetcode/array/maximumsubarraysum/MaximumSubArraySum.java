b897200cd96c8df4dad14d7d30fdafcd0da73cd8
adnanrahin <adnan.rahin13@gmail.com>
2024-10-24 20:07:06 -0400

    Maximum SubArray Sum

---
+++ src/online/judge/leetcode/array/maximumsubarraysum/MaximumSubArraySum.java
@@ -0,0 +1,30 @@
+package online.judge.leetcode.array.maximumsubarraysum;
+
+public class MaximumSubArraySum {
+
+    public static void main(String[] args) {
+
+        System.out.println(maxSubArray(new int[]{-2, 1, -1, 2, 3, -5}));
+
+    }
+
+    public static int maxSubArray(int[] nums) {
+
+        if (nums.length == 0) return 0;
+
+        int globalMax = nums[0], localMax = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (localMax + nums[i] < nums[i]) {
+                localMax = 0;
+            }
+            localMax += nums[i];
+            globalMax = Math.max(localMax, globalMax);
+
+        }
+        return globalMax;
+
+    }
+
+}
