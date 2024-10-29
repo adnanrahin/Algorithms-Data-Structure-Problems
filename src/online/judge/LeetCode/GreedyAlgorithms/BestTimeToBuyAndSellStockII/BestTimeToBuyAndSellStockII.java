b860563f624831d94a20e9170add11f598b4d672
Adnan Habib Rahin <adnan.rahin13@gmail.com>
2024-10-29 02:36:58 -0400

    Best Time to Buy Stock Two

--- src/online/judge/LeetCode/GreedyAlgorithms/BestTimeToBuyAndSellStockII/BestTimeToBuyAndSellStockII.java
+++ src/online/judge/LeetCode/GreedyAlgorithms/BestTimeToBuyAndSellStockII/BestTimeToBuyAndSellStockII.java
@@ -4,6 +4,10 @@ public class BestTimeToBuyAndSellStockII {
 
     public static void main(String[] args) {
 
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfit(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+
     }
 
     public static int maxProfit(int[] prices) {
@@ -15,7 +19,10 @@ public class BestTimeToBuyAndSellStockII {
             if (buy > prices[i]) {
                 buy = prices[i];
             }
-            maxProfit = Math.max(maxProfit, prices[i] - buy);
+            if (prices[i] - buy > 0) {
+                maxProfit += (prices[i] - buy);
+                buy = prices[i];
+            }
         }
 
         return maxProfit;
